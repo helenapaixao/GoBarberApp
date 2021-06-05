6adcbbfc4cda0499d7c1a7434e0cc1359d5fc5b6
Helena Paixão <hp.helenapaixao@gmail.com>
2021-06-05 19:40:45 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import {View, StatusBar} from 'react-native';
+
+const App: React.FC = () => (
+  <>
+  <StatusBar barStyle="light-content" backgroundColor="#312E38" />
+  <View  style={{ flex:1,backgroundColor:'#312E38'}}/>
+</>
+) 
+
+export default App;
